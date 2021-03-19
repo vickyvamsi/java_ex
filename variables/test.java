350033de7e2e14c27bbe0dbf1fb9a3f21b84860a
vickyvamsi <gvkrishna10@gmail.com>
2021-03-19 14:35:13 +0530

    first commit

---
+++ variables/test.java
@@ -0,0 +1,17 @@
+class Test
+{
+	int a=10;
+	int b=20;
+	public static void main(String[] args) 
+	{ 
+		Test t=new Test();
+		System.out.println(t.a);
+		System.out.println(t.b);
+	} 
+	    void m1()
+	{
+			System.out.println(a);
+		    System.out.println(a);
+}
+
+}
